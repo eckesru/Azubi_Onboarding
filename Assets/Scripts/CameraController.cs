6c73b2692b8274404f41a524af5904a96153b0c9
eckesru <rudi.eckes@gmail.com>
2024-01-15 21:57:37 +0100

    PlayerConoller inkl. SlopeMovement implementiert

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private float rotationSpeed = 100.0f;
+
+    private float xRotation;
+    private float yRotation;
+    private float mouseX;
+    private float mouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCameraRotation();
+    }
+
+    private void HandleCameraRotation()
+    {
+        // Holen der Mausbewegung (X und Y-Achse) und Anpassung an Speed
+        mouseX = Input.GetAxisRaw("Mouse X") * rotationSpeed * Time.fixedDeltaTime;
+        mouseY = Input.GetAxisRaw("Mouse Y") * rotationSpeed * Time.fixedDeltaTime;
+
+        // Berechnung der Aenderung der Blickrichtung
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        // Ausfuehren der Aenderung der Blickrichtung
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float walkMovementSpeed = 70.0f;
+    [SerializeField] private float runMovementSpeed = 120.0f;
+    [SerializeField] private float dragForce = 10.0f;
+
+    private float movementSpeed;
+
+    private float horizontal;
+    private float vertical;
+    private Vector3 direction;
+
+    private float slopeAngle = 40.0f;
+    private RaycastHit slopeHit;
+
+    private float playerHeight;
+
+    public Transform cameraView;
+
+    [SerializeField] private GameManager gameManager;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        playerHeight = cameraView.position.y;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Zentriert den Cursor in der Mitte und blendet ihn aus
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        HandleMovementSpeed();
+
+        HandleMovementOnSlope();
+
+        HandlePlayerMovement();
+
+    }
+
+    private bool OnSlope() { 
+
+        // Pruefung, ob Player sich auf einem Slope befindet
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < slopeAngle && angle != 0;
+        }
+        return false;
+    }
+
+    private Vector3 GetSlopeDirection()
+    {
+        // Holt die Richtung des Slopes.
+        return Vector3.ProjectOnPlane(direction, slopeHit.normal).normalized;
+    }
+
+    private void HandleMovementSpeed()
+    {
+        // Anpassen von movementSpeed bei LeftShift zum Rennen
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = runMovementSpeed;
+        }
+        else
+        {
+            movementSpeed = walkMovementSpeed;
+        }
+    }
+
+    private void HandleMovementOnSlope()
+    {
+        // Handhabung von der Bewegung des Spielers auf einem Slope
+        // Schwerkraft wird auf Slopes deaktiviert, um Rutschen zu verhindern
+        if (OnSlope())
+        {
+            // Verlangsamung des Spielers auf Slopes
+            movementSpeed = movementSpeed * 0.6f;
+
+            // Anpassung der Bewegungskraft in die Richtung des Slopes
+            rb.AddForce(GetSlopeDirection() * movementSpeed, ForceMode.Force);
+
+            rb.useGravity = false;
+
+            if (rb.velocity.y < 0)
+            {
+                // Beim Absteigen von Slopes wird der Spieler heruntergedrueckt
+                rb.AddForce(Vector3.down * 100.0f, ForceMode.Force);
+            }
+        }
+        else
+        {
+            rb.useGravity = true;
+        }
+    }
+
+    private void HandlePlayerMovement()
+    {
+        // Generelle Handhabung der Bewegung des Spielers
+
+        // Holen der Eingabe durch WASD/Pfeiltasten
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        // Berechnet die Bewegung anhand der Blickrichtung der Kamera
+        direction = cameraView.forward * vertical + cameraView.right * horizontal;
+
+        // Verhindert Veraenderung der Höhe (Fliegen), wenn die Kamera nach oben/unten schaut
+        direction.y = 0;
+
+        // Fuehrt die Bewegung in die Blickrichtung der Kamera aus
+        rb.AddForce(direction.normalized * movementSpeed, ForceMode.Force);
+
+        // Fixiert die Ziehkraft des Spielers auf einen fixen Wert (Verhindert Rutschen auf dem Boden)
+        rb.drag = dragForce;
+    }
+}
